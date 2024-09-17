bc77c76a341e6c86257fb81f90b0cd3a295a7f2e
zare <zarellaburbanov@gmail.com>
2024-09-16 23:20:08 -0500

    controllers payments y products

--- payments/controllers/payments.controller.js
+++ payments/controllers/payments.controller.js
@@ -6,17 +6,50 @@
 *Este archivo define los controladores de pagos
 */
 const {response,request} = require ('express')
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 
 const ProcessPayments = async(req=request, res=response)=>{
+
+    const { amount, method, date } = req.body;
+
+    const result = await prisma.users.create({
+        data: {
+            amount,
+            method,
+            date
+        }
+    }).catch(err=>{
+        return err.message;
+    }).finally((async ()=>{
+        await prisma.$disconnect();
+    }));
+
     res.json({
-        "saludo":"Soy la respuesta de procesar pagos"
+        result
     });
 
 };
 
 const ReturnPayment = async(req=request, res=response)=>{
+
+    const { amount, method, date } = req.body;
+
+    const result = await prisma.users.create({
+        data: {
+            amount,
+            method,
+            date
+        }
+    }).catch(err=>{
+        return err.message;
+    }).finally((async ()=>{
+        await prisma.$disconnect();
+    }));
+
     res.json({
-        "saludo":"Soy la respuesta de retornar pago"
+        result
     });
 
 };
--- products/controllers/products.controller.js
+++ products/controllers/products.controller.js
@@ -8,26 +8,78 @@
 const {response,request} = require ('express')
 
 const AddProduct = async(req=request, res=response)=>{
+
+
+    const { name, description, price, category } = req.body;
+
+    const result = await prisma.users.create({
+        data: {
+            name,
+            description,
+            price,
+            category
+        }
+    }).catch(err=>{
+        return err.message;
+    }).finally((async ()=>{
+        await prisma.$disconnect();
+    }));
+
     res.json({
-        "saludo":"Soy la respuesta de añadir producto"
+        result
     });
+
 };
 
 const UpdateProduct = async(req=request, res=response)=>{
+    const { name, description, price, category } = req.body;
+
+    const result = await prisma.users.create({
+        data: {
+            name,
+            description,
+            price,
+            category
+        }
+    }).catch(err=>{
+        return err.message;
+    }).finally((async ()=>{
+        await prisma.$disconnect();
+    }));
+
     res.json({
-        "saludo":"Soy la respuesta de actualizar producto"
+        result
     });
 };
 
 const DeleteProduct = async(req=request, res=response)=>{
+    const { id } = req.params;
+
+    const result = await prisma.users.delete({
+        where:{
+            id: Number(id)
+        }
+    }).catch(err=>{
+        return err.message;
+    }).finally((async ()=>{
+        await prisma.$disconnect();
+    }));
+
     res.json({
-        "saludo":"Soy la respuesta de eliminar producto"
+        result
     });
 };
 
 const GetProduct = async(req=request, res=response)=>{
+    const products = await prisma.products.findMany()
+    .catch(err=>{
+        return err.message;
+    }).finally((async ()=>{
+        await prisma.$disconnect();
+    }));
+
     res.json({
-        "saludo":"Soy la respuesta de obtener producto"
+        products
     });
 };
 
