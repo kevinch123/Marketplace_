1b8ba57d1524127385538c7225c4b0f8c92c207e
kevinchi <kevinchi78@gmail.com>
2024-10-04 11:49:14 -0500

    asd

--- orders/controllers/order.controller.js
+++ orders/controllers/order.controller.js
@@ -7,11 +7,9 @@
  */
 
 const { response, request } = require('express');
-<<<<<<< HEAD
-const prisma = require ('../../prisma/prismaClient')
-=======
+
 const prisma = require('../../prisma/prismaClient.js');
->>>>>>> 991890096a8735151738aee27547a498883be4a0
+
 
 const ShowOrders = async (req = request, res = response) => {
     try {
@@ -31,14 +29,12 @@ const AddOrders = async (req = request, res = response) => {
     try {
         const { total, orderDate, products, payments, shoppingCarId } = req.body;
 
-        // Verificar si se envió la información necesaria
         if (!total || !orderDate || !products || !payments || !shoppingCarId) {
             return res.status(400).json({
                 message: 'Faltan datos requeridos para crear la orden',
             });
         }
 
-        // Crear la orden
         const newOrder = await prisma.orders.create({
             data: {
                 total: total,
@@ -47,12 +43,11 @@ const AddOrders = async (req = request, res = response) => {
                 // Crear el pago relacionado
                 payments: {
                     create: {
-                        method: payments.method, // Ajuste aquí para usar 'method'
+                        method: payments.method, 
                         amount: payments.amount,
-                        date: new Date() // Agregar la fecha del pago
+                        date: new Date() 
                     },
                 },
-                // Asociar los productos a la orden
                 products: {
                     connect: products.map((productId) => ({ id: productId })),
                 },
@@ -67,7 +62,7 @@ const AddOrders = async (req = request, res = response) => {
         console.error(error);
         return res.status(500).json({
             message: 'Error al crear la orden',
-            error: error.message, // Opcional: devolver mensaje de error
+            error: error.message, 
         });
     }
 };
@@ -75,4 +70,4 @@ const AddOrders = async (req = request, res = response) => {
 module.exports = {
     AddOrders,
     ShowOrders
-};
+};  
